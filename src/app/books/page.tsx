62ad0534a49ba0758f590ff5affa29586fd9a40f
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-20 01:17:20 +0600

    inplement navbar and create books page

---
+++ src/app/books/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BooksPage = () => {
+    return (
+        <div>
+            <h1>Books</h1>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default BooksPage;
