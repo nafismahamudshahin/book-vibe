07a91b4671d15f28b04469fb667d6cd022a6d4a4
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-09-21 14:32:20 +0600

    sorted readed and wishlist books by rating,year and pages

--- src/app/listed-books/page.tsx
+++ src/app/listed-books/page.tsx
@@ -1,13 +1,36 @@
 "use client";
 import ListedBooksCard from '@/components/BooksDetails/ListedBooksCard';
 import { booksContext } from '@/context/booksContext';
-import React, { useContext } from 'react';
+import { IBook } from '@/types/books.type';
+import React, { useContext, useState } from 'react';
 
 const ListedBooksPage = () => {
+
     const { readedBooks, wishList } = useContext(booksContext)
+    const [selectSort, setSelectSort] = useState("");
+    const booksSorted = (books: IBook[]): IBook[] => {
+        const booksForSort = [...books];
+        if (selectSort === "rating") {
+            return booksForSort.sort((a, b) => b.rating - a.rating);
+        } else if (selectSort === "year") {
+            return booksForSort.sort((a, b) => b.yearOfPublishing - a.yearOfPublishing);
+        }
+        return booksForSort.sort((a, b) => b.totalPages - a.totalPages);
+    }
+    const sorteRededBooks = booksSorted(readedBooks);
+    const sorteWishListBooks = booksSorted(wishList);
     return (
         <div>
             <h1 className='text-center text-3xl font-bold py-3'> books</h1>
+            <div>
+                {/* sort */}
+                <select onChange={(e) => setSelectSort(e.target.value)} defaultValue="Pick a language" className="select select-secondary">
+                    <option disabled={true}>Pick a language</option>
+                    <option value="rating">by rating </option>
+                    <option value="year">by year</option>
+                    <option value="pages">by pages</option>
+                </select>
+            </div>
             <div>
                 {/* name of each tab group should be unique */}
                 <div className="tabs tabs-box">
@@ -16,7 +39,7 @@ const ListedBooksPage = () => {
                         <div className='grid grid-cols-1 gap-5'>
 
                             {
-                                readedBooks.map(book => <ListedBooksCard key={book.bookId} book={book}></ListedBooksCard>)
+                                sorteRededBooks.map(book => <ListedBooksCard key={book.bookId} book={book}></ListedBooksCard>)
                             }
                         </div>
                     </div>
@@ -26,7 +49,7 @@ const ListedBooksPage = () => {
                         <div className='grid grid-cols-1 gap-5'>
 
                             {
-                                wishList.map(book => <ListedBooksCard key={book.bookId} book={book}></ListedBooksCard>)
+                                sorteWishListBooks.map(book => <ListedBooksCard key={book.bookId} book={book}></ListedBooksCard>)
                             }
                         </div>
                     </div>
